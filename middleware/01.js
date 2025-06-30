0b3115a44e6e3c877cd7478fa18c07701aff3ebe
Manasvi Nawal <manasvinawal28@gmail.com>
2025-06-30 10:53:14 +0530

    added middleware

---
+++ middleware/01.js
@@ -0,0 +1,24 @@
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of requests made to the server in the global
+// requestCount variable
+
+const express = require('express');
+const app = express();
+
+let count = 0;
+app.use((req, res, next) => {
+    count += 1;
+    next();
+});
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount', function(req, res) {
+  res.status(200).json({ requestCount });
+});
