04ff3709d038bf7c51ce324846c702e993056808
Manasvi Nawal <manasvinawal28@gmail.com>
2025-07-07 18:45:10 +0530

    fixed some code

--- todo/todoBackend.js
+++ todo/todoBackend.js
@@ -2,64 +2,58 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const todo = new Map();
+const todos = new Map();
 let id = 0;
 
-function generateID(){
+function generateID() {
     return (++id).toString();
 }
 
-function addTodo(req, res){
-    //get the title
-    const {title} = req.body;
-    if(!title){
-        return res.status(400).json({message : "You need to enter your todo"});
+function addTodo(req, res) {
+    const { title } = req.body;
+    if (!title) {
+        return res.status(400).json({ message: "You need to enter your todo" });
     }
     const todo = {
-        id : generateID(),
+        id: generateID(),
         title,
-        completed: false
+        completed: false,
     };
-    //map -> id : todo
-    // set is used to map the corresponding id to the todo
-    //todos.set(todo.id, todo)
     todos.set(todo.id, todo);
-    res.json({message : "Your todo has been added"});
+    res.status(201).json({ message: "Your todo has been added", todo });
 }
 
-function getAllTodos(req, res){
-    //return all the todos
+function getAllTodos(req, res) {
     res.json(Array.from(todos.values()));
 }
 
-function deleteAllTodos(req, res){
-    //clear the map 
-    const {id} = req.params;
+function deleteTodoById(req, res) {
+    const { id } = req.params;
 
-    if(!todos.has(id)){
-        return res.status(404).json({message : "This todo has not been mentioned"});
-
+    if (!todos.has(id)) {
+        return res.status(404).json({ message: "This todo does not exist" });
     }
     todos.delete(id);
-    res.json({message : "Todo has been deleted successfully"});
+    res.json({ message: "Todo has been deleted successfully" });
 }
 
-function completeToDo(req, res){
-    const {id} = req.params;
-    if(!todos.has(id)){
-        return res.status(404).json({message : "to do not found"});
+function completeToDo(req, res) {
+    const { id } = req.params;
+    if (!todos.has(id)) {
+        return res.status(404).json({ message: "Todo not found" });
     }
 
     const todo = todos.get(id);
-    todos.completed = true;
+    todo.completed = true;
     todos.set(id, todo);
-    res.json({message : "Updation has been done"});
+    res.json({ message: "Todo updated", todo });
 }
-app.post('/todo', addTodo);
-app.get('/todos', getAllTodos);
-app.delete('/todos/:id', deleteAllTodos);
-app.put("/todos/:id", completeToDo);
 
-app.listen(3001, () =>{
-    console.log("Server is running")
-})
+app.post('/todos', addTodo);
+app.get('/todos', getAllTodos);
+app.delete('/todos/:id', deleteTodoById);
+app.put('/todos/:id', completeToDo);
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
